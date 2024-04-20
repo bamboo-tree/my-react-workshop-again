7f9bf2b7d5cf5e76b060b267ed2df314d0d59f67
Maciej Kaminski <maciej.kam03@gmail.com>
2024-04-20 13:39:12 +0200

    Created Text component using tailwind and storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import { Text } from "./ui";
 
 function App() {
 	return (
 		<>
-			<h1 className="text-pink-300 bg-black px-16 py-4 rounded-full">
-				Elo bync
-			</h1>
+			<Text>Elo bync</Text>
 		</>
 	);
 }
